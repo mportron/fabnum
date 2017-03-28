e58c891ef280c6ea3aecfdc158ccc5892efad3ce
patjoub <contact@patjoub.net>
2017-03-28 22:47:11 +0200

    petite modif présentation datas.php

--- atelier2/exercice_regex/datas.php
+++ atelier2/exercice_regex/datas.php
@@ -1,18 +1,18 @@
 <?php
 $meteo=array(
-	'Poitiers'=>array(
-	    'temperature'=>15,
+    'Poitiers'=>array(
+        'temperature'=>15,
         'icone'=>'http://www.icone-png.com/png/11/10685.png'
     ),
-	'Niort'=>array(
+    'Niort'=>array(
         'temperature'=>25,
         'icone'=>'http://www.icone-png.com/png/11/10690.png'
     ),
-	'La Rochelle'=>array(
+    'La Rochelle'=>array(
         'temperature'=>-15,
         'icone'=>'http://www.icone-png.com/png/11/10701.png'
     ),
-	'Paris'=>array(
+    'Paris'=>array(
         'temperature'=>5,
         'icone'=>'http://www.icone-png.com/png/11/10628.png'
     )

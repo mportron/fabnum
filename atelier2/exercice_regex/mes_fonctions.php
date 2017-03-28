692ba2766a672d261c84c718055774ffe93abd73
patjoub <contact@patjoub.net>
2017-03-28 22:47:57 +0200

    petite modif présentation mes_fonctions.php

--- atelier2/exercice_regex/mes_fonctions.php
+++ atelier2/exercice_regex/mes_fonctions.php
@@ -8,13 +8,13 @@
 function afficheTemperature(string $ville, array $detail){
 
     $temperature=$detail['temperature'];
-	$chaine="La température de {ville} est de {temperature} degrés";
-	$chaine=preg_replace('/{ville}/',$ville,$chaine);
-	$chaine=preg_replace('/{temperature}/',$temperature,$chaine);
+    $chaine="La température de {ville} est de {temperature} degrés";
+    $chaine=preg_replace('/{ville}/',$ville,$chaine);
+    $chaine=preg_replace('/{temperature}/',$temperature,$chaine);
 
-	$icone='<img src="'.$detail['icone'].'" width="150" alt="le temps"/>';
+    $icone='<img src="'.$detail['icone'].'" width="150" alt="le temps"/>';
 
-	return ['phrase'=>$chaine,'icone'=>$icone];
+    return ['phrase'=>$chaine,'icone'=>$icone];
 }
 
 
